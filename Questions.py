73b34340c21a55a86eda40f03c35e36141b93457
Abhilash <abhinaani2005@gmail.com>
2025-04-21 19:33:01 +0000

    Msg committed

---
+++ Questions.py
@@ -0,0 +1,45 @@
+# Import necessary libraries
+
+#Import Dataset - set1_data.csv 
+
+# Print top 5 and random 5 values - 2 Marks
+
+#Print column names - 1 Marks
+
+#Check the data type of each column - 1 Marks
+
+#Print number of rows and columns 1 Mark
+
+#Print summary statistics of all variables - 1 Mark
+
+#Print the number of missing values in each column - 1 Marks
+
+#Impute missing values - Replace missing categorical values with mode and numerical values with mean of the column - 3 Mark
+#Print the number of missing values in each column after imputation
+
+#Print count of patients in each category - 1 Marks
+
+#Plot histogram of values of patient's perceived comfort at discharge - 1 Marks
+
+# Plot barchart of L-CORE, L-SURF,L-O2 and L-BP using 4X4 subplots - 3 Marks
+#num_bins = 10
+
+# Plot pie chart of patient's surface temperature in C - 2 Mark
+
+#Prepare X - input matrix and y - label vector - 1 Mark
+
+#Apply label encoder for categorical columns - 2 Marks
+
+# Split the data into train and test set  - use test size = 30% and random state = 4 : - 2 Marks
+
+
+# Fit the Naive Bayes classifier on training data - 3 Marks
+
+
+# Make prediction on test data - 1 Marks
+
+# Check the evaluation metrics of the model - Confusion Matrix, Accuracy, Precision, Recall, F1 score - 3 Marks
+
+
+
+#Apply prediction on any one sample input and print the probability estimates for each class - 1 marks
